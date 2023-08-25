243f789510acf194f6ebf5431dc9e4b06b5bc18a
Eric Li <ELi2687341@gmail.com>
2023-08-25 10:58:48 -0400

    update

---
+++ mod4_solution/script.js
@@ -0,0 +1,20 @@
+var names=new Array();
+names[0]="Danny";
+names[1]="James";
+names[2]="Frank";
+names[3]="Eric";
+names[4]="Megan";
+names[5]="Jeffrey";
+names[6]="Sadie";
+names[7]="Jacob";
+names[8]="Yaakov";
+names[9]="Jade";
+
+for (var i = 0; i < names.length; i++) {
+    if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='J') {
+        console.log("Goodbye "+ names[i])
+    }
+    else{
+        console.log("Hello "+ names[i])
+    }
+}
